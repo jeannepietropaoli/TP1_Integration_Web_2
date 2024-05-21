706a7e6c17235119337f932341a58800feb62aea
Jeanne Pietropaoli <j.pietropaoli@yahoo.fr>
2024-05-21 10:37:33 -0400

    Finalisation de la validation du formulaire

--- formValidation.js
+++ formValidation.js
@@ -1,47 +1,93 @@
-const firstNameInput = document.getElementById('firstName')
-const lastNameInput = document.getElementById('lastName')
-const emailInput = document.getElementById('email')
-const messageInput = document.getElementById('message')
+const firstNameInput = document.getElementById("firstName");
+const lastNameInput = document.getElementById("lastName");
+const emailInput = document.getElementById("email");
+const messageInput = document.getElementById("message");
+
+const inputs = [
+  {
+    input: firstNameInput,
+    errorMessage: "Veuillez indiquer votre prenom.",
+    validationFunction: isFirstNameValid,
+  },
+  {
+    input: lastNameInput,
+    errorMessage: "Veuillez indiquer votre nom.",
+    validationFunction: isLastNameValid,
+  },
+  {
+    input: emailInput,
+    errorMessage:
+      "Votre courriel doit contenir au moins 4 caracteres dont un '@'.",
+    validationFunction: isEmailValid,
+  },
+  {
+    input: messageInput,
+    errorMessage: "Ecrivez-nous au moins un petit quelque chose ;)",
+    validationFunction: isMessageValid,
+  },
+];
 
 function getInputValue(input) {
-    return input.value.trim();
+  return input.value.trim();
 }
 
 function isInputFilled(inputValue) {
-    return inputValue !== '' || inputValue.length > 0
+  return inputValue !== "" || inputValue.length > 0;
 }
 
 function isLastNameValid() {
-    const lastName = getInputValue(lastNameInput)
-    return isInputFilled(lastName) && lastName.length <= 25
+  const lastName = getInputValue(lastNameInput);
+  return isInputFilled(lastName) && lastName.length <= 25;
 }
 
 function isFirstNameValid() {
-    const firstName = getInputValue(firstNameInput)
-    return isInputFilled(firstName) && firstName.length <= 25
+  const firstName = getInputValue(firstNameInput);
+  return isInputFilled(firstName) && firstName.length <= 25;
 }
 
 function isEmailValid() {
-    const email = getInputValue(emailInput)
-    return isInputFilled(email) && email.length >= 5 && firstName.length <= 40 && email.includes('@')
+  const email = getInputValue(emailInput);
+  return (
+    isInputFilled(email) &&
+    email.length >= 5 &&
+    email.length <= 40 &&
+    email.includes("@")
+  );
 }
 
 function isMessageValid() {
-    const message = getInputValue(messageInput)
-    return isInputFilled(message)
+  const message = getInputValue(messageInput);
+  return isInputFilled(message);
 }
 
-function setSuccess() {
-
+function getErrorMessageElement(input) {
+  const parentNode = input.parentElement;
+  return parentNode.querySelector(".form__error-msg");
 }
 
-function setError() {
-    
+function setSuccess(input) {
+  const errorDisplay = getErrorMessageElement(input);
+  errorDisplay.textContent = "";
+  input.classList.remove("form__input--error");
+  input.classList.add("form__input--success");
+}
+
+function setError(input, errorMessage) {
+  const errorDisplay = getErrorMessageElement(input);
+  errorDisplay.textContent = errorMessage;
+  input.classList.remove("form__input--success");
+  input.classList.add("form__input--error");
 }
 
 function validateForm() {
-    isLastNameValid() ? setSuccess(lastNameInput) : setError(lastNameInput, 'Veuillez indiquer votre nom.')
-    isFirstNameValid() ? setSuccess(firstNameInput) : setError(firstNameInput, 'Veuillez indiquer votre prenom.')
-    isEmailValid() ? setSuccess(emailInput) : setError(emailInput, 'Votre courriel doit contenir au moins 4 caracteres dont un \'@\'.')
-    isMessageValid() ? setSuccess(messageInput) : setError(messageInput, 'Ecrivez-nous au moins un petit quelque chose ;)')
+  let readyToSend = true;
+  inputs.forEach((input) => {
+    if (input.validationFunction()) {
+      setSuccess(input.input);
+    } else {
+      setError(input.input, input.errorMessage);
+      readyToSend = false;
+    }
+  });
+  return readyToSend;
 }
