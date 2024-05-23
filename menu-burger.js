46297862177d9999759ee20cec8b04050377c8f3
Jeanne Pietropaoli <j.pietropaoli@yahoo.fr>
2024-05-22 22:02:22 -0400

    Menu mobile: Ajout de la logique d'apparition au clic

---
+++ menu-burger.js
@@ -0,0 +1,9 @@
+const menuBurgerButton = document.querySelector('.nav__burger-button');
+console.log(menuBurgerButton)
+const nav = document.querySelector('.nav--desktop');
+
+menuBurgerButton.addEventListener('click', () => {
+    nav.style.display = nav.style.display === 'inline-block' ? 'none' : 'inline-block';
+    nav.classList.toggle('nav--desktop')
+    nav.classList.toggle('nav--mobile');
+})
