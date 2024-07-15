e33d7d62e0f64f607b1919f39c55752f01209065
Jeanne Pietropaoli <j.pietropaoli@yahoo.fr>
2024-07-15 19:51:15 -0400

    Modification du service-worker

--- service-worker.js
+++ service-worker.js
@@ -7,7 +7,7 @@ const FILES_TO_CACHE = [
     'index.html'
 ];
 
-// Installation
+// Installation - fait suelement une fois, premiere ouverture du site (installation du service worker)
 self.addEventListener("install", (evt) => {
     console.log('[ServiceWorker] Install');
     // Precache static resources here.
@@ -20,17 +20,39 @@ self.addEventListener("install", (evt) => {
     self.skipWaiting();
 });
 
-// Activation
-self.addEventListener("activate", (evt) => {
-    
-  console.log("[ServiceWorker] Activate");
-  //Remove previous cached data from disk.
-  self.clients.claim();
-});
+// Activation -  a chaque ouverture du site / app
+self.addEventListener('activate', (evt) => {
+    console.log('[ServiceWorker] Activate');
+    //Remove previous cached data from disk.
+    evt.waitUntil(
+    caches.keys().then((keyList) => {
+    return Promise.all(keyList.map((key) => {
+    if (key !== CACHE_NAME) {
+    console.log('[ServiceWorker] Removing old cache',
+   key);
+    return caches.delete(key);
+    }
+    }));
+    })
+    );
+    self.clients.claim();
+   })
 
 //Acces aux ressources
-self.addEventListener("fetch", (evt) => {
-    // Precache static resources here.
-  console.log("[ServiceWorker] Fetch", evt.request.url);
-  //Add fetch event handler here.
-});
+self.addEventListener('fetch', (evt) => {
+    console.log('[ServiceWorker] Fetch', evt.request.url);
+    //Add fetch event handler here.
+    if (evt.request.mode !== 'navigate') {
+    // Not a page navigation, bail.
+    return;
+    }
+    evt.respondWith(
+    fetch(evt.request)
+    .catch(() => {
+    return caches.open(CACHE_NAME)
+    .then((cache) => {
+   return cache.match('/Cochenille/PointNClick/offline.html' );
+    });
+    })
+    );
+   });
