628c73da31c9b62387d6e1577389f346676d6309
Jeanne Pietropaoli <j.pietropaoli@yahoo.fr>
2024-07-15 19:30:32 -0400

    Modification du service-worker

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,7 @@ const CACHE_NAME = "static-cache-v1";
 //Add list of files to cache here.
 const FILES_TO_CACHE = [
     'offline.html',
-    
+    'index.html'
 ];
 
 // Installation
@@ -22,15 +22,6 @@ self.addEventListener("install", (evt) => {
 
 // Activation
 self.addEventListener("activate", (evt) => {
-    
-  console.log("[ServiceWorker] Activate");
-  //Remove previous cached data from disk.
-  self.clients.claim();
-});
-
-//Acces aux ressources
-self.addEventListener("fetch", (evt) => {
-    // Precache static resources here.
     evt.waitUntil(
         caches.open(CACHE_NAME).then((cache) => {
         console.log('[ServiceWorker] Pre-caching offline page');
@@ -38,6 +29,15 @@ self.addEventListener("fetch", (evt) => {
         })
         );
         self.skipWaiting();
+  console.log("[ServiceWorker] Activate");
+  //Remove previous cached data from disk.
+  self.clients.claim();
+  
+});
+
+//Acces aux ressources
+self.addEventListener("fetch", (evt) => {
+    // Precache static resources here.
   console.log("[ServiceWorker] Fetch", evt.request.url);
   //Add fetch event handler here.
 });
