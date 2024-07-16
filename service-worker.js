87ff3c779b7ba7d974f7b31555cb5b2b8da8d6ae
Jeanne Pietropaoli <j.pietropaoli@yahoo.fr>
2024-07-15 20:00:54 -0400

    Modification du service worker

--- service-worker.js
+++ service-worker.js
@@ -51,7 +51,7 @@ self.addEventListener('fetch', (evt) => {
     .catch(() => {
     return caches.open(CACHE_NAME)
     .then((cache) => {
-   return cache.match('/Cochenille/PointNClick/offline.html' );
+   return cache.match('offline.html' );
     });
     })
     );
